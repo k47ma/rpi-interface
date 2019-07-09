f9ea106bc5065f19b6eb1c69d652572578f85bbd
Kai Ma <kaima2019@fb.com>
2019-07-09 12:06:55 -0700

    Added snake move

--- lib/games.py
+++ lib/games.py
@@ -56,6 +56,10 @@ class GameSnake(Game):
         self._board_x = self._score_width + (self._screen_width - self.total_cols * self.cell_size) // 2
         self._board_y = (self._screen_height - self.total_rows * self.cell_size) // 2
         self._board = []
+        self._snake = []
+        self._snake_directon = "right"
+        self._snake_speed = 3
+        self._snake_lastmove = time.time()
         self._started = False
         self._start_time = time.time()
         self._cell_color = self._get_color("lightgray")
@@ -74,6 +78,10 @@ class GameSnake(Game):
         if not self.is_active or not self._started:
             return
 
+        current_time = time.time()
+        if current_time - self._snake_lastmove >= 1 / self._snake_speed:
+            self._snake_move()
+
     def _on_draw(self, screen):
         if not self.is_active:
             return
@@ -88,14 +96,27 @@ class GameSnake(Game):
                 if not self._started:
                     self._start_game()
 
+            if self._started:
+                if self._snake_directon in ["up", "down"]:
+                    if event.key == pygame.K_LEFT:
+                        self._snake_directon = "left"
+                    elif event.key == pygame.K_RIGHT:
+                        self._snake_directon = "right"
+                else:
+                    if event.key == pygame.K_UP:
+                        self._snake_directon = "up"
+                    elif event.key == pygame.K_DOWN:
+                        self._snake_directon = "down"
+
     def _start_game(self):
         self._started = True
         self._start_time = time.time()
 
     def _init_board(self):
         self._board = [[0 for _ in range(self.total_cols)] for _ in range(self.total_rows)]
-        self._board[self.total_rows // 2][self.total_cols // 2] = 1
-        self._board[self.total_rows // 2][self.total_cols // 2 + 1] = 1
+        self._snake = []
+        self._snake.append((self.total_rows // 2, self.total_cols // 2 + 1))
+        self._snake.append((self.total_rows // 2, self.total_cols // 2))
         self._score = 0
         self._started = False
         self._add_apple()
@@ -104,10 +125,10 @@ class GameSnake(Game):
         free_spots = []
         for row_ind in range(self.total_rows):
             for col_ind in range(self.total_cols):
-                if self._board[row_ind][col_ind] == 0:
+                if self._board[row_ind][col_ind] not in self._snake:
                     free_spots.append((row_ind, col_ind))
         row, col = random.choice(free_spots)
-        self._board[row][col] = 2
+        self._board[row][col] = 1
 
     def _draw_scoreboard(self, screen):
         score_text = self.scoreboard_font.render("Score: {}".format(self._score), True, self._get_color("white"))
@@ -128,10 +149,24 @@ class GameSnake(Game):
             for col_ind in range(self.total_cols):
                 if self._board[row_ind][col_ind] == 0:
                     color = self._cell_color
-                elif self._board[row_ind][col_ind] == 1:
-                    color = self._snake_color
                 else:
                     color = self._apple_color
+                if (row_ind, col_ind) in self._snake:
+                    color = self._snake_color
                 x = self._board_x + col_ind * self.cell_size + self.cell_padding
                 y = self._board_y + row_ind * self.cell_size + self.cell_padding
                 pygame.draw.rect(screen, color, (x, y, rect_size, rect_size))
+
+    def _snake_move(self):
+        snake_row, snake_col = self._snake[0]
+        if self._snake_directon == "left":
+            snake_col = self.total_cols - 1 if snake_col == 0 else snake_col -1
+        elif self._snake_directon == "right":
+            snake_col = 0 if snake_col == self.total_cols - 1 else snake_col + 1
+        elif self._snake_directon == "up":
+            snake_row = self.total_rows - 1 if snake_row == 0 else snake_row - 1
+        else:
+            snake_row = 0 if snake_row == self.total_rows - 1 else snake_row + 1
+
+        self._snake = [(snake_row, snake_col)] + self._snake[:-1]
+        self._snake_lastmove = time.time()
