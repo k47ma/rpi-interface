41a2dcbf956e927cae5a57bd9c58c970d425aa8a
Kai Ma <kaima2019@fb.com>
2019-08-22 17:22:59 -0700

    Added new test case

--- lib/test.py
+++ lib/test.py
@@ -17,3 +17,14 @@ class TestUtils:
         assert not in_sorted(-1, list1)
         assert not in_sorted(5, list1)
         assert not in_sorted(100, list1)
+
+    def test_bytes_to_string(self):
+        assert bytes_to_string(0) == "0B"
+        assert bytes_to_string(100) == "100B"
+        assert bytes_to_string(1000) == "1.00K"
+        assert bytes_to_string(1024) == "1.02K"
+        assert bytes_to_string(10240) == "10.24K"
+        assert bytes_to_string(1000 ** 2) == "1.00M"
+        assert bytes_to_string(1.5 * 1000 ** 2) == "1.50M"
+        assert bytes_to_string(1000 ** 3) == "1.00G"
+        assert bytes_to_string(100.3 * 1000 ** 3) == "100.30G"
